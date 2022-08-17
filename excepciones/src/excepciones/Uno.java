6f2d60be91f1756461373ea2a1f2cb46d53937a1
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2022-08-17 12:35:53 -0300

    fix and add comments

--- excepciones/src/excepciones/Uno.java
+++ excepciones/src/excepciones/Uno.java
@@ -11,20 +11,20 @@ class Uno {
         int valor = 0;
         try {
             valor = valor + 1;  // Incrementa en 1 es decir valor = 1
-            valor = valor + Integer.parseInt("ABC");    // Lanza error y se dirige al catch
+            valor = valor + Integer.parseInt("ABC");    // Lanza error y se dirige al catch con valor = 1
             valor = valor + 1;
             System.out.println("Valor al fin al del try: " + valor);
         } catch (NumberFormatException e) {
-            // Entra al catch
-            //valor = valor + Integer.parseInt("ABC");
+            // Entra al catch con valor = 1
             System.out.println("Valor al final del catch: " + valor);
             // Como no hay error imprime el resultado de valor que era 1
         } finally {
-            // Ejecuta finally incrementando en 1 a la variable valor
+            // Ejecuta finally incrementando en 1 a la variable valor, debido a que catch capturo anteriormente la excepcion 
             valor = valor + 1;  // entonces valor vale 2
             System.out.println("Valor al final de finally : " + valor);
         }
-        valor = valor + 1;
+        // Termina de ejecutar el try/catch capturando la expecion en el catch, el finally y continua con las siguientes lineas de codigo
+        valor = valor + 1;  // Por ultimo valor vale 3
         System.out.println("Valor antes del return: " + valor);
         return valor;
     }
