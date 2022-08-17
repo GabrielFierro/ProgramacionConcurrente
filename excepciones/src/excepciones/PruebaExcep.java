3cfae483e797aee1bea6a3359d4a50cf13985544
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2022-08-17 18:21:33 -0300

    fix: fix the exceptions in the ruleta en edadPersona methods

--- excepciones/src/excepciones/PruebaExcep.java
+++ excepciones/src/excepciones/PruebaExcep.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package excepciones;
+
 import java.util.Scanner;
 
 /**
@@ -11,21 +12,41 @@ import java.util.Scanner;
  * @author gabriel.fierro
  */
 public class PruebaExcep {
-    private static void edadPersona(){
+
+    private static void edadPersona() {
         int edad = 0;
         Scanner sc = new Scanner(System.in);
-        try{
+        try {
             System.out.println("Ingrese la edad de la persona");
-            edad = sc.nextInt(); 
-            if(edad < 18){
-                throw new Exception();
+            edad = sc.nextInt();
+            if (edad < 18) {
+                throw new RuntimeException("La persona es menor de edad");
             }
-        }catch(Exception e){
-            System.out.println("Es menor de edad");
+        } catch (RuntimeException e) {
+            throw e;
         }
     }
-    
+
+    private static void ruleta() {
+        int valor = 0, min = 1, max = 20, numeroRandom;
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Ingrese un valor");
+            valor = sc.nextInt();
+            numeroRandom = (int) Math.floor(Math.random() * (max - min + 1) + min);
+            System.out.println("El numero random es: " + numeroRandom);
+            if (valor != numeroRandom) {
+                throw new RuntimeException("El numero ingresado no coincide con el de la ruleta");
+            }else{
+                System.out.println("Acertaste el numero");
+            }
+        } catch (RuntimeException e) {
+            throw e;
+        }
+    }
+
     public static void main(String[] args) {
-        edadPersona();
+        //edadPersona();
+        ruleta();
     }
 }
