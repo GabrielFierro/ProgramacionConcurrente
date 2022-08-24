1ccac850a9010a79e2544584cb4b3aa00c196c97
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2022-08-24 14:50:14 -0300

    add: add a project with the Cliente, Recurso and TesteoRecursos classes

---
+++ hilos/src/hilos/TesteoRecurso.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hilos;
+
+/**
+ *
+ * @author gabriel.fierro
+ */
+public class TesteoRecurso {
+    public static void main(String [] args){
+        // Declaro y creo 2 objetos de tipo Cliente
+        Cliente juan = new Cliente();
+        juan.setName("Juan Lopez");
+        Cliente ines = new Cliente();
+        ines.setName("Ines Garcia");
+        
+        juan.start();   // inicia juan concurrentemente con main(). Invoca a juan.run() y vuelve inmediatamente
+        ines.start();   // idem comentario de arriba
+        Recurso.uso();
+    }
+    
+}
