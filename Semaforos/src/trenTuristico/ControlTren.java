88fe82ef7e3e1f958988a45d9990f8ec17a06efa
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2022-11-09 15:05:10 -0300

    add: add the Constructor, the run and the puedenSubir methods

--- Semaforos/src/trenTuristico/ControlTren.java
+++ Semaforos/src/trenTuristico/ControlTren.java
@@ -10,5 +10,24 @@ package trenTuristico;
  * @author gabriel.fierro
  */
 public class ControlTren extends Thread{
+    private Cabina cab;
+    private int cantAsientos;
     
+    public ControlTren(Cabina cab, int cantAsientos){
+        this.cab = cab;
+        this.cantAsientos = cantAsientos;
+    }
+    
+    @Override
+    public void run(){
+        System.out.println("Controla si los pasajeros pueden subir al Tren");
+    }
+    
+    public void puedenSubir(){
+        boolean exito;
+        exito = this.cab.puedenSubir(cantAsientos);
+        if(exito){
+            this.cab.subirPasajeros();
+        }
+    }
 }
