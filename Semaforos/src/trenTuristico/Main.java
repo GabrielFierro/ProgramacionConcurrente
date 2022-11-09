9ca18adaa70325c959fd2b30d170453612e714b7
GabrielFierro <gabriel.fierro@est.fi.uncoma.edu.ar>
2022-11-09 15:03:36 -0300

    add the main class that test the other classes in the package

--- Semaforos/src/trenTuristico/Main.java
+++ Semaforos/src/trenTuristico/Main.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package trenTuristico;
+
 import colores.Colores;
+
 /**
  *
  * @author gabriel.fierro
@@ -17,15 +19,27 @@ public class Main {
         Colores c2 = new Colores("cyan");
         Colores c3 = new Colores("violeta");
 
+//        int cantTickets = 3;
+//        int cantPasajeros = 4;
+//        int cantAsientos = 10;
+//        1 pasajero queda fuera y no puede subir
+//        int cantTickets = 5;
+//        int cantPasajeros = 4;
+//        int cantAsientos = 10;
+//        todos los pasajeros pueden comprar ticket y subir al tren
         int cantTickets = 3;
         int cantPasajeros = 4;
+        int cantAsientos = 10;
+
         Colores colors[] = {c0, c1, c2, c3};
         Cabina cab = new Cabina(cantTickets);
+        ControlTren ct = new ControlTren(cab, cantAsientos);
         Pasajero[] pasajeros = new Pasajero[cantPasajeros];
-        Vendedor v = new Vendedor("Jorge", cab, cantPasajeros);
+        Vendedor v = new Vendedor("Jorge", cab, cantTickets);
 
         for (int i = 0; i < cantPasajeros; i++) {
-            pasajeros[i] = new Pasajero(colors[i].pintar("Pasajero " + (i)), cab);
+            pasajeros[i] = new Pasajero(colors[i].pintar("Pasajero " + (i)), cab, ct);
+            pasajeros[i].setName("Pasajero " + (i));
         }
 
         for (int i = 0; i < cantPasajeros; i++) {
